c0b81178612d355ed3ce429d38cf4f3a8e6b9ae7
VicYaelPM <yaelproo2712@gmail.com>
2024-04-18 11:43:10 -0600

    feature/Se elimina get

--- src/main/java/com/apirest/apirest/ApirestApplication.java
+++ src/main/java/com/apirest/apirest/ApirestApplication.java
@@ -21,7 +21,7 @@ public class ApirestApplication {
 				@Override
 				public void addCorsMappings(CorsRegistry registry) {
 					registry.addMapping("/**")
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+							.allowedMethods("HEAD", "PUT", "POST", "DELETE", "PATCH");
 				}
 			};
 		}
