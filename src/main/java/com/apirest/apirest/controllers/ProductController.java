e56cd7e145ab95b4192314dea915e3f346de8e03
VicYaelPM <yaelproo2712@gmail.com>
2024-04-18 11:55:25 -0600

    se eliminan coors

--- src/main/java/com/apirest/apirest/controllers/ProductController.java
+++ src/main/java/com/apirest/apirest/controllers/ProductController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
-
 @RestController
 @RequestMapping("/products")
 public class ProductController {
